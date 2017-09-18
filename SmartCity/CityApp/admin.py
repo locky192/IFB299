75e67e78060bdcf510b18b3316cf85c783b322bd
kdcruz98 <kyletristan.delacruz@connect.qut.edu.au>
2017-09-18 17:45:46 +1000

    Final Sprint 1 I hope

--- SmartCity/CityApp/admin.py
+++ SmartCity/CityApp/admin.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from CityApp.models import UserProfile
+from CityApp.models import UserProfile, CityInfo
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(CityInfo)
+
--- SmartCity/CityApp/models.py
+++ SmartCity/CityApp/models.py
@@ -5,6 +5,39 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class CityInfo (models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    landmark_type = models.CharField(blank=True, max_length=30)
+
+    business = 'Business'
+    student = 'Student'
+    tourist = 'Tourist'
+
+    user_type_choices = (
+        (business, 'Business'),
+        (student, 'Student'),
+        (tourist, 'Tourist')
+    )
+
+    user_type = models.CharField(
+        max_length = 8,
+        choices = user_type_choices,
+        default = business,
+        )
+    
+    industry_type = models.CharField(blank=True, max_length=30)
+    phone_number = models.CharField(blank=True, max_length = 15)
+    unit_number = models.CharField(blank=True, max_length = 3)
+    street_number = models.CharField(blank=True, max_length = 3)
+    suburb = models.CharField(blank=True, max_length = 20)
+    state = models.CharField(blank=True, max_length = 20)
+    postcode = models.CharField(blank=True, max_length = 4)
+    email = models.CharField(blank=True, max_length = 100)
+
+    def __str__(self):
+        return self.name
+
 
 class UserProfile (models.Model):
     user = models.OneToOneField(User)
